3c9b38c97fc464a902dc4ced199c0d56d1e81d03
maximser <maximser1974@gmail.com>
2020-05-31 23:42:31 +0300

    fix typos

--- Lab8/src/main/java/org/maximser/Bank.java
+++ Lab8/src/main/java/org/maximser/Bank.java
@@ -6,41 +6,29 @@ import java.util.ArrayList;
 public class Bank {
     private String name;
     private ArrayList<Account> accounts = new ArrayList<>();
-    private int lastId;
 
     public Bank(String name) {
         this.name = name;
-        lastId = 0;
     }
 
     public void addAccount(Account account) {
         accounts.add(account);
-        lastId++;
     }
 
     public void transfer(Account from, Account to, int amount) {
-        if (from.getId() > from.getId()) {
+        synchronized (from) {
             synchronized (to) {
-                synchronized (from) {
-                    if (from.withdraw(amount))
-                        to.deposit(amount);
-                }
-            }
-        } else {
-            synchronized (from) {
-                synchronized (to) {
-                    if (from.withdraw(amount))
-                        to.deposit(amount);
-                }
+                if (from.withdraw(amount))
+                    to.deposit(amount);
             }
         }
     }
 
     public long getTotalBalance() {
-        long ans = 0;
+        long bankBalance = 0;
         for (Account account : accounts)
-            ans += account.getBalance();
-        return ans;
+            bankBalance += account.getBalance();
+        return bankBalance;
     }
 
     public ArrayList<Account> getAccounts() {
@@ -49,10 +37,6 @@ public class Bank {
 
     public String getName() {
         return name;
-    }
-
-    public int getLastId() {
-        return lastId;
     }
 
 }
